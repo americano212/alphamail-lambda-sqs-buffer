3e8cfd70a4279f56e8b2d9364f7715e6444154e5
DongJun Shin <wq0212@naver.com>
2024-06-28 16:30:23 +0900

    feat: Add connect Mysql

--- src/lambda-sqs-trigger/lambda_function.py
+++ src/lambda-sqs-trigger/lambda_function.py
@@ -1,16 +1,28 @@
 import json
 import os
 import boto3
+from mysql.connector import (connection)
 
 
 def lambda_handler(event, context):
+    config = {
+        'user': os.environ['user'],
+        'password': os.environ['password'],
+        'host':  os.environ['host'],
+        'database':  os.environ['database'],
+    }
+    cnx = connection.MySQLConnection(**config)
+
     lst = [{'email_id': [1, 2]}]
     msg_body = json.dumps(lst)
-    print("msg_body", msg_body)
+    
     msg = send_sqs_message(os.environ['SQS_QUEUE'], msg_body)
-    print("msg", msg)
+
+    cnx.close()
     
 def send_sqs_message(sqs_queue_url, msg_body):
     sqs_client = boto3.client('sqs')
-
     msg = sqs_client.send_message(QueueUrl=sqs_queue_url, MessageBody=msg_body)
+
+# lambda_handler(None, None)
+
