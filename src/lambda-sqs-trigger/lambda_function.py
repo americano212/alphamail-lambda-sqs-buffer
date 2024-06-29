6984f7349db72632aa0c17d73b33cfd390ee4804
DongJun Shin <wq0212@naver.com>
2024-06-29 16:12:52 +0900

    feat: Change logic that send message to SQS

--- src/lambda-sqs-trigger/lambda_function.py
+++ src/lambda-sqs-trigger/lambda_function.py
@@ -15,23 +15,25 @@ def lambda_handler(event, context):
     cursor = cnx.cursor()
 
     query = '''
-    SELECT id as email_id FROM mail WHERE is_spam IS NULL LIMIT 10
+    SELECT id as email_id FROM mail WHERE is_spam IS NULL LIMIT 100
     '''
     cursor.execute(query)   
     select_all_result = cursor.fetchall()
     cnx.close()
 
     lst = [x[0] for x in select_all_result]
-    result = [{'email_id': lst}]
-    msg_body = json.dumps(result)
-    
-    msg = send_sqs_message(os.environ['SQS_QUEUE'], msg_body)
+
+    for idx in range(0, len(lst), 10):
+        result = [{'email_id': lst[idx:idx+10]}]
+        msg_body = json.dumps(result)
+        msg = send_sqs_message(os.environ['SQS_QUEUE'], msg_body)
 
     
     
 def send_sqs_message(sqs_queue_url, msg_body):
     sqs_client = boto3.client('sqs')
     msg = sqs_client.send_message(QueueUrl=sqs_queue_url, MessageBody=msg_body)
+    return msg
 
 # lambda_handler(None, None)
 
