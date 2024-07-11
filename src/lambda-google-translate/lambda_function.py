278691c28f5dcd13e2249bdf1f2ce71bc8162e9c
DongJun Shin <wq0212@naver.com>
2024-07-11 23:35:36 +0900

    feat: translate in consumer

--- src/lambda-google-translate/lambda_function.py
+++ src/lambda-google-translate/lambda_function.py
@@ -40,13 +40,13 @@ def lambda_handler(event, context):
             'headers': {'Content-Type': 'application/json'}
         }
 
-event = {
-  'queryStringParameters' :{
-      "sourceWord": "안녕하세요. 나는 홍길동입니다.",
-      "sourceLanguage": "ko",
-      "targetLanguage": "en"
-  }
-}
+# event = {
+#   'queryStringParameters' :{
+#       "sourceWord": "안녕하세요. 나는 홍길동입니다.",
+#       "sourceLanguage": "ko",
+#       "targetLanguage": "en"
+#   }
+# }
 
-context = {}
-print(lambda_handler(event, context))
+# context = {}
+# print(lambda_handler(event, context))
--- src/lambda-sqs-consumer/lambda_function.py
+++ src/lambda-sqs-consumer/lambda_function.py
@@ -29,7 +29,7 @@ def lambda_handler(event, context):
 
             for email_id in email_id_list:
                 content = get_email_content(email_id, cursor)
-
+                translate(content)
                 payload = spam_classification(content)
                 statusCode = payload['statusCode']
                 body = payload['body']
@@ -40,8 +40,8 @@ def lambda_handler(event, context):
                 else:
                     errorMessage = body
                     save_error_is_spam(email_id, errorMessage, cursor, cnx)
-
         cnx.close()
+
     except Exception as e:
         print('Error', e)
 
@@ -64,6 +64,16 @@ def spam_classification(content)->dict:
     payload = json.loads(response['Payload'].read().decode('utf8').replace("'", '"'))
     return payload
 
+def translate(content)->str:
+    lambda_client = boto3.client('lambda')
+    response = lambda_client.invoke(    
+        FunctionName='lambda-google-translate',
+        InvocationType='RequestResponse',
+        Payload=json.dumps({"body-json": {"queryStringParameters": {"sourceWord": content, "sourceLanguage": "ko", "targetLanguage": "en"}}})
+    )
+    payload = json.loads(response['Payload'].read().decode('utf8').replace("'", '"'))
+    print("translate_payload-----------", payload)
+
 def save_is_spam(email_id: int, is_spam: int, cursor, cnx):
     query='''
     UPDATE mail SET is_spam={0} WHERE id={1}
@@ -83,5 +93,5 @@ def save_error_is_spam(email_id: int, error_message: str, cursor, cnx):
     print("email_id", email_id)
     print("error_message", error_message)
 
-# event = {'Records': [{'messageId': 'b5e3a71c-2d6c-4cb1-b63f-a40140e79ecb', 'receiptHandle': 'AQEBvSo2nlu1BtFaHF4tJHPKnnsZxr3P+8rfQ3Ol+Im//gZsB8Lx/NWhPBfgw8h1lZbLuC5Gbb7xQecplAIaMN+tL2nxvr/67hq0+rutAvpq9Z6jKUC3eX7LYLoFPERhAx9bzTOnxTnU2EoQZ7fKH4LERAiVISB9eCMQcVn6we43cR1xU6uKVy+KfurSoH8Jg+Q7NCrF4YWAsePy6iB+SGd7Ck1CTfpGOVJRZ7qOsVsLe6vIGcHCgANeNeT6uoGO1TroZR0XJZ95N7ntQ/IfIsR1KpB35MOyeo+JEpMed2FtPpjP165QfImmbzcCCsTXEGQ01YHDCeXYlPUVfrw/h+X7XtYDDrdk9t0CzWlzc+aENCna2PSYAtNe86HdSFZX07XtG1gHz8mWPQM1Sef5k4sxt/+PHLfCsPTvAMp7M62gFtw=', 'body': '[{"email_id": [1, 2]}]', 'attributes': {'ApproximateReceiveCount': '1', 'AWSTraceHeader': 'Root=1-667e5c03-53329ab12355f3d24289a171;Parent=4592baba5fa057e6;Sampled=0;Lineage=c6c7d02b:0', 'SentTimestamp': '1719557125833', 'SenderId': 'AROA47CRWGZ7UE5PZBWTJ:lambda-sqs-trigger', 'ApproximateFirstReceiveTimestamp': '1719557125835'}, 'messageAttributes': {}, 'md5OfBody': '7826165d70deae7e440711f70a2723d7', 'eventSource': 'aws:sqs', 'eventSourceARN': 'arn:aws:sqs:ap-northeast-2:891377038975:sqs-alphamail-spam-classification', 'awsRegion': 'ap-northeast-2'}]}
-# lambda_handler(event, "context")
+event = {'Records': [{'body': {'email_id': [1]}}]}
+lambda_handler(event, "context")
